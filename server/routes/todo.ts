09bf1d25ddc75daaeffb8038738f606cddaffc83
jean claude <jean@DESKTOP-8RHST5R.localdomain>
2022-11-21 11:08:35 +0100

    first commit

--- server/routes/todo.ts
+++ server/routes/todo.ts
@@ -1,40 +1,75 @@
-import express,{Request,Response} from "express";
+import express, {Request, Response} from 'express';
 
-import todomModel from "../models/todoModel";
+import todoModel from '../models/todoModel';
 
 let Router = express.Router();
 
-Router.post('/', async(request: Request, response: Response) => {
-    const { label, description } = request.body;
+Router.post('/', async (request: Request, response: Response) => {
+    const {label, description} = request.body
 
-    let newTodo = new todomModel({label, description});
-    try{
-    let todo= await newTodo.save();
-    response.status(200).json(todo);
-    }catch(error){
-    response.status(500).json(error);
+    let newTodo = new todoModel({
+        label,
+        description
+    })
+
+    try {
+        let todo = await newTodo.save()
+        return response.status(200).json(todo)
+    } catch(error) {
+        return response.status(500).json({"msg": error})
     }
-    return response.status(200).json({"msg": "Create todo page !"});
 });
 
 Router.get('/', (request: Request, response: Response) => {
-    return response.status(200).json({"msg": "List  todo page !"});
-    });
-
-Router.get('/:todoId', async(request: Request, response: Response) => {
-    let {todoId} = request.params;
-    try{
-        let todo= await todomModel.findById(todoId);
-        response.status(200).json(todo);
-        }catch(error){
-        return response.status(500).json(error);
-        }
+    response.status(200).json({"msg": "List todos page !"})
 });
 
-Router.delete('/', (request: Request, response: Response) => {
-    let {todoId} = request.params;
-    return response.status(200).json({"msg": "The Delete" + todoId+ "todo page !"});
+Router.get('/:todoId', async (request: Request, response: Response) => {
+    let { todoId } = request.params;
+    
+    try {
+        let todo = await todoModel.findOne({
+            _id: todoId
+        });
+
+        return response.status(200).json(todo);
+    } catch(error) {
+        return response.status(500).json({msg: error});
+    }
 });
 
+Router.delete('/:todoId', async (request: Request, response: Response) => {
+    let { todoId } = request.params;
+
+    try {
+        await todoModel.findOneAndDelete({
+            _id: todoId
+        });
+
+        return response.status(200).json({"msg": "Todo well deleted !"});
+    } catch (error) {
+        return response.status(500).json({msg: error});
+    }
+});
+
+Router.put('/:todoId', async (request: Request, response: Response) => {
+    let { todoId } = request.params;
+    const {label, description} = request.body
+
+    try {
+        let todo = await todoModel.findOneAndUpdate({
+            _id: todoId
+        }, {
+            label,
+            description
+        }, {
+            new: true
+        });
+
+        return response.status(200).json(todo);
+    } catch (error) {
+        return response.status(500).json({msg: error});
+    }
+});
 
 export default Router;
